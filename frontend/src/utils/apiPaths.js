948201505c635a0f710643f726b994e4fdb0bec4
Anaghwalia <anaghwalia50@gmail.com>
2025-08-31 16:11:55 +0530

    Update API base URL in apiPaths.js for deployment

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,5 +1,4 @@
-export const BASE_URL = 'http://localhost:4000'
-
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
 // Routes used for frontend
 
 export const API_PATHS = {
